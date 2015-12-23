f7834910d193a6d886b67e9b5a35ead08eaf7d71
Michele Memoli <michele@100shapes.com>
2015-12-23 13:08:07 +0000

    Passed props to checked component

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
 import Cycle from '@cycle/core';
 import {Observable} from 'rx';
-import {makeDOMDriver} from '@cycle/dom';
+import {makeDOMDriver, div} from '@cycle/dom';
 
 import Checker from './Components/Checker';
 
 const Main = (sources) => {
 
+    const props$ = Observable.of({
+        label: 'Mega toggle'
+    });
+
+    const checker = Checker({
+        DOM: sources.DOM,
+        props$
+    });
+
+    const isChecked$ = checker.isChecked$;
+    const checkerVTree$ = checker.DOM;
+
+    const vtree$ = checkerVTree$.withLatestFrom(isChecked$, (checkerVTree, isChecked) =>
+        div([
+            checkerVTree,
+            div(`Checked? ${isChecked}`)
+        ])
+    );
+
     return {
-        DOM: Checker(sources).DOM
+        DOM: vtree$
     };
 };
 
 Cycle.run(Main, {
-    DOM: makeDOMDriver('#app'),
-    props$: () => {
-        return Observable.of({
-            label: 'Mega toggle'
-        });
-    }
+    DOM: makeDOMDriver('#app')
 });
